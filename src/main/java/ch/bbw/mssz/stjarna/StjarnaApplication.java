008fa29a94b4f070469e0b72193b884309019d43
MatthiasStierlin <mbstierlin@gmail.com>
2020-11-13 09:01:38 +0100

    Initial commit

---
+++ src/main/java/ch/bbw/mssz/stjarna/StjarnaApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.mssz.stjarna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StjarnaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StjarnaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/bbw/mssz/stjarna/StjarnaApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.bbw.mssz.stjarna;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StjarnaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
